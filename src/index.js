f2775376745b968e9ad55cb16c2d530c4218e5df
jeremylombogia <jeremylombogia7@gmail.com>
2018-07-30 16:22:24 +0700

    Change endpoint

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:3333/";
+axios.defaults.baseURL = "http://178.128.19.182:3333/";
 axios.defaults.withCredentials = true;
 
 ReactDOM.render(<App />, document.getElementById("root"));
